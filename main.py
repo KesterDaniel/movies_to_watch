a3bc75bcde03c459c6d696715082ed91c50d82ac
KesterDaniel <kesterdan17@gmail.com>
2023-03-03 16:09:36 +0100

    adding movies to txt file

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ webpage = response.text
 
 soup = BeautifulSoup(webpage, "html.parser")
 
+movie_titles = [title.getText() for title in soup.find_all(name="h3", class_="title")]
+
+for i in range(1, len(movie_titles)+1):
+    with open("movies.txt", "a", encoding="utf-8") as movie_file:
+        movie_file.write(f"{movie_titles[-i]}\n")
+
+
+
 
 
 
